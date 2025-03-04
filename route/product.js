dd3dc65525036bc7fec14bda3da07a47c81414bd
sumaiya-rahman-chowdhury <bdfara@1>
2025-03-05 05:52:49 +0600

    Get a specific product by ID

--- route/product.js
+++ route/product.js
@@ -31,5 +31,17 @@ router.post("/add-product", async (req, res) => {
     res.status(500).json({ message: "Error adding product", error });
   }
 });
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving product", error });
+  }
+});
+
 
 module.exports = router;
