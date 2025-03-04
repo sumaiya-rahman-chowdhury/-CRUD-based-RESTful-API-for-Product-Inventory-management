472845866e8d740ae142ecd7c3b2bf5d7e499279
sumaiya-rahman-chowdhury <bdfara@1>
2025-03-05 05:39:19 +0600

    post routes done

--- route/product.js
+++ route/product.js
@@ -10,5 +10,26 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+router.post("/add-product", async (req, res) => {
+  try {
+    const { name, price, category, stock, description } = req.body;
+    if (!name || !price || !category || !stock) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const newProduct = new Product({
+      name,
+      price,
+      category,
+      stock,
+      description,
+    });
+    await newProduct.save();
+    res
+      .status(201)
+      .json({ message: "Product added successfully", product: newProduct });
+  } catch (error) {
+    res.status(500).json({ message: "Error adding product", error });
+  }
+});
 
 module.exports = router;
