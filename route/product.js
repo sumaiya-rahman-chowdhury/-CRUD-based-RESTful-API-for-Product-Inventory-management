c3bac51431397d95286f8af4856c7b8901bc58e6
sumaiya-rahman-chowdhury <bdfara@1>
2025-03-05 02:42:40 +0600

    Project Setup & Dependencies and MongoDB Schema & Model

---
+++ route/product.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/Product");
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const productsRoutes = require("./route/product");
+const { default: mongoose } = require("mongoose");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use("/products", productsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
