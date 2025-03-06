21899857673364f2a55cbc91075b46c5440ab279
sumaiya-rahman-chowdhury <bdfara@1>
2025-03-06 22:30:43 +0600

    Done

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { default: mongoose } = require("mongoose");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 mongoose
   .connect(MONGO_URI, {
@@ -18,7 +18,9 @@ mongoose
   })
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
+app.get("/", async(req, res) => {
+  res.json({ message: "Welcome To Api" });
+});
 app.use("/api/products", productsRoutes);
 app.use("/api/register", registerRouter);
 app.use("/api/login", loginRouter);
@@ -26,3 +28,5 @@ app.use("/api/login", loginRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
